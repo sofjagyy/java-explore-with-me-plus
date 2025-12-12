fc414b413b2dfd29415fef42e5972ebc11995f16
sofjagyy <sofjagy@yandex.ru>
2025-12-12 17:39:22 +0100

    remove exception duplication in compilation

--- main-service/src/main/java/ru/practicum/compilation/exception/CompilationNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.compilation.exception;
-
-public class CompilationNotFoundException extends RuntimeException {
-
-    public CompilationNotFoundException(String message) {
-        super(message);
-    }
-}
